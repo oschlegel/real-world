60c601c5b2c2bf3997a457f9f6ce66ccfaff89ca
Oliver Schlegel <11634412+oschlegel@users.noreply.github.com>
2021-07-06 13:37:21 +0200

    add angular-akita app

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/app-next', '<rootDir>/libs/models'],
+  projects: [
+    '<rootDir>/apps/app-next',
+    '<rootDir>/libs/models',
+    '<rootDir>/apps/app-angular-akita',
+  ],
 };
